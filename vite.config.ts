f15f2703b5b6cb10944d0065e22fa9b0af1318e8
Melon <xmelosx@live.jp>
2026-06-24 20:36:35 +0900

    デフォルトポート変更

--- vite.config.ts
+++ vite.config.ts
@@ -98,7 +98,7 @@ export default defineConfig({
   ],
   server: {
     host: true,
-    port: 5173,
+    port: 29980,
     watch: {
       ignored: [
         path.resolve(__dirname, 'global_markers.json'),
